304663ba41eaa49927425236e53dadf1060c78a3
Pickles <pickles@pop-os.localdomain>
2021-03-01 13:33:41 -0500

    first and final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [author, setAuthor ] = useState(null);
+  const [text, setText ] =  useState(null)
+
+  const handleClick = () => {
+    fetch('https://freequote.herokuapp.com/')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setAuthor(data.author);
+        setText(data.quote);
+      })
+  }
+
+  useEffect(() => {
+    fetch('https://freequote.herokuapp.com/')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setAuthor(data.author);
+        setText(data.quote);
+      })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <div className="text-and-author">
+        <p id="text">{ text }</p>
+        <p id="author">{ author }</p>
+      </div>
+      <div className="buttons-wrapper">
+      <a id='tweet-quote' title='Tweet Quote' className='tweet-btn' href="twitter.com/intent/tweet" target='_blank'><i className="fab fa-twitter"></i></a>
+      <button onClick={handleClick} id="new-quote"><span className='add'>+</span> New Quote</button>
+      </div>
     </div>
   );
 }
